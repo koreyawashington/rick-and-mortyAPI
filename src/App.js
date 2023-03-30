bda51a85d072306f382bf9a6bc136859150f3ec6
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-03-30 09:41:59 -0400

    updating main

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 //Importing javascript in bootstrap
 import "bootstrap/dist/js/bootstrap";
+//Import React, useState, and useEffect from react
+import React, {useState, useEffect} from "react";
 
 //Importing components
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
+import Search from "./components/Search/Search"
+import Pagination from "./components/Pagination/Pagination"
+import NavBar from "./components/NavBar/NavBar"
 
 
 function App() {
-  
-  const api ='https://rickandmortyapi.com/api/character'
+  //===========This is the section fot eh main logic of the page numbers and how they will change======
+  //The number inside of the useState(parentheses) is a default placeholder for the page number
+  const [pageNumber, setPageNumber ] = useState(1);
+  //In order to use other  variables inside of the api weblink I must use back ticks instead of single or double quotes
+  const api =`https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  //===================================================================================================
+
+//=========Fetching Data=================
+
+useEffect(() => {
+
+(async function(){
+  const data = await fetch(api).then(res=>res.json())
+  console.log(data);
+})()
+
+  //When api changes I want to fetch new data inside of the arrow function
+}, [api]) 
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span></h1>
