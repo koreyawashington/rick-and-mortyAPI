b7da08e3e6292114dece25e76361bf4b68857af7
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-04-01 17:00:25 -0400

    power outage

--- src/App.js
+++ src/App.js
@@ -8,26 +8,39 @@ import React, {useState, useEffect} from "react";
 //Importing components
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
-import Search from "./components/Search/Search"
-import Pagination from "./components/Pagination/Pagination"
-import NavBar from "./components/NavBar/NavBar"
+// import Search from "./components/Search/Search"
+// import Pagination from "./components/Pagination/Pagination"
+// import NavBar from "./components/NavBar/NavBar"
 
 
 function App() {
   //===========This is the section fot eh main logic of the page numbers and how they will change======
   //The number inside of the useState(parentheses) is a default placeholder for the page number
+  //when the page changes the pageNumber will update to setPageNumber and render that data.
   const [pageNumber, setPageNumber ] = useState(1);
+  //This state will hold our character information
+  const [fetchedData, updateFetchedData] = useState([]);
+  const {info, results} = fetchedData
+ 
+  //console.log will allow me to see the results of each new page to ensure everything is running properly
+  // console.log(results);
+  
+
+  //The api key is stored in my const api variable
   //In order to use other  variables inside of the api weblink I must use back ticks instead of single or double quotes
   const api =`https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
   //===================================================================================================
 
 //=========Fetching Data=================
-
+//when the website loads for the first time it will automatically render the data for the default page(page 1)
+//when the page clicks over to a new page, new data will render using the useEffect statement below
+//This function will run as soon as the page changes to show the results of the newly rendered data from the api
 useEffect(() => {
 
 (async function(){
   const data = await fetch(api).then(res=>res.json())
-  console.log(data);
+ updateFetchedData(data)
+  // console.log(data.results);
 })()
 
   //When api changes I want to fetch new data inside of the arrow function
@@ -46,7 +59,7 @@ useEffect(() => {
           {/* The 'Cards' div will consists of 8 columns */}
           <div className="col-8">
             <div className="row">
-            <Cards />
+            <Cards results={results}/>
             
             </div>
           </div>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import styling from './Cards.module.scss'
 
-const Cards = () => {
-  return (
-    <div className='col-4'>Cards</div>
-  )
+
+const Cards = ({results}) => {
+    
+    let display;
+    console.log(results);
+    
+    if(results){
+        //the x at the start of the arrow function will target the results one at a time.
+        display = results.map((x)=>{
+            //now I will destructure the x
+            let{id, name, image, location, status} = x;
+            return(
+            <div key={id} className="col-4 position-relative">
+                <div className="">
+                    <img src={image} alt="" className="img-fluid" />
+                    <div className="content">
+                        <div className="fs-4 fw-bold mb-4">{name}</div>
+                        <div className="">
+                            <div className="fs-6">Last Known Location</div>
+                            <div className="fs-5">{location.name}</div>
+                            </div>         
+                    </div>
+                </div>
+               <div className={`styling.badge`}"position-absolute badge bg-danger">{status}</div>
+            </div>
+            );
+        });
+    }else{
+        display = "No Characters Found :/;"
+    }
+  
+    return <>{display}</>
+  
 }
 
 export default Cards
