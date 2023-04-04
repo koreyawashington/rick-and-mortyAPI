3142cc9c4cc9021d95eec35a175085555e636afe
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-04-03 20:44:37 -0400

    projectMeetsMinimunRequirementsNowIamJustStyling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -9,12 +9,12 @@ const Search = () => {
     <form className="d-flex justify-content-right gap-4 mb-5">
 {/* The input is where the web user will be able to use the search bar to search throughout the website */}
       <input 
-      placeholder='Wubba Lubba Dub Dub ' 
+      placeholder='Wubba Lubba Dub Dub!' 
       type="text" 
       className={styling.input}
       />
 
-      <button className="btn btn-primary">Search</button>
+      <button className={`btn btn-primary fs-5`}>Search</button>
     
     </form>
   )
