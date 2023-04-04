107601e7a5bc4a33ef52bf4780f4e49233211dde
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-04-04 05:45:33 -0400

    search bar working

--- src/App.js
+++ src/App.js
@@ -15,15 +15,26 @@ import Pagination from "./components/Pagination/Pagination"
 // import NavBar from "./components/NavBar/NavBar"
 
 
+
 function App() {
-  //===========This is the section fot eh main logic of the page numbers and how they will change======
+
+  //===========This is the section for the main logic of the page numbers and how they will change=================================================
   //The number inside of the useState(parentheses) is a default placeholder for the page number.
   //When the page changes pageNumber will update to setPageNumber and render that data.
   // Inside of the const variable is a function, pageNumber is classified as a variable and setPageNumber is classified as a function.
   // setPageNumber is used to change the pageNumber === very simple
   const [pageNumber, setPageNumber ] = useState(1);
-  
-console.log(pageNumber);
+  console.log(pageNumber);
+  // ====================================================================================================================
+
+
+
+  //================This is the controls for the Search Box==============================================================
+  //The search is the variable and the setSearch is the function
+  //This function in this useState statement will be passed down to the Search component in App.js
+  const [search, setSearch] = useState("")
+  //=====================================================================================================================
+
   
   //This state will hold our character information
   const [fetchedData, updateFetchedData] = useState([]);
@@ -31,15 +42,16 @@ console.log(pageNumber);
   const {info, results} = fetchedData
  
   //console.log will allow me to see the results of each new page to ensure everything is running properly
-  // console.log(results);
+  console.log(results);
   
 
   //The api key is stored in my const api variable
   //In order to use other  variables inside of the api weblink I must use back ticks instead of single or double quotes
-  const api =`https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
-  //===================================================================================================
+  //also adding how to search(filter through) for a specific character to the end of the api which is $name=${search}
+  const api =`https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  //====================================================================================================================
 
-//=========Fetching Data=================
+//=========Fetching Data================================================================================================
 //when the website loads for the first time it will automatically render the data for the default page(page 1)
 //when the page clicks over to a new page, new data will render using the useEffect statement below
 //This function will run as soon as the page changes to show the results of the newly rendered data from the api
@@ -48,7 +60,7 @@ useEffect(() => {
 (async function(){
   const data = await fetch(api).then(res=>res.json())
  updateFetchedData(data)
-  // console.log(data.results);
+  console.log(data.results);
 })()
 
   //When api changes I want to fetch new data inside of the arrow function
@@ -59,7 +71,8 @@ useEffect(() => {
       {/* This is the styling for the banner text at the top of the webpage */}
       <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span></h1>
 
-      <Search  />  
+      {/* here is where the setSearch function is passed into the Search component */}
+      <Search setSearch={setSearch}/>
 
       <div className="container">
         <div className="row">
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,17 +4,21 @@ import React from 'react'
 import styling from "./Search.module.scss";
 
 
-const Search = () => {
+const Search = ({ setSearch }) => {
   return (
     <form className="d-flex justify-content-right gap-4 mb-5">
 {/* The input is where the web user will be able to use the search bar to search throughout the website */}
+      {/* When there is a change in events or when something is submitted inside the search input box that will trigger the setSearch function to run. After the setSearch function is triggered it will update the search variable and the update the api which means that it will run the useEffect to get fresh new data that will be related to the what was being searched for in the input search box*/}
       <input 
+      onChange={e=>{
+        setSearch(e.target.value);
+      }}
       placeholder='Wubba Lubba Dub Dub!' 
       type="text" 
       className={styling.input}
       />
 
-      <button className={`btn btn-primary fs-5`}>Search</button>
+      <button className={`${styling.btn} btn btn-primary fs-5`}>Search</button>
     
     </form>
   )
